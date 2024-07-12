54b1a5e81818d8c0e547c43f5a240b051ecb8cc9
hakeemee1 <91174671+hakeemee1@users.noreply.github.com>
2024-07-12 11:21:05 +0700

    Post method

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello,ffff World!")
+	})
+
+
+	
+	type Person struct {
+		Name string `json:"name"`
+		Pass string `json:"pass"`
+	}
+	app.Post("/", func(c *fiber.Ctx) error {
+		p := new(Person)
+
+		if err := c.BodyParser(p); err != nil {
+			return err
+		}
+
+		log.Println(p.Name) // john
+		log.Println(p.Pass) // doe
+		str := p.Name + p.Pass
+		return c.JSON(str)
 	})
 
 	app.Listen(":3000")
 }
-
-
-
-
