6a9f2b814a3cf5a2447057f267c24fd1079d2cb3
hakeemee1 <91174671+hakeemee1@users.noreply.github.com>
2024-07-15 10:47:46 +0700

    basic auth middleware

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"log"
 )
 
 func main() {
 	app := fiber.New()
-
+	// Provide a minimal config
+	app.Use(basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"john":  "doe",
+			"admin": "123456",
+		},
+	}))
+	
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello,ffff World!")
+		return c.SendString("Hello,ffff World!") //SendString is Ctx method
 	})
 
-
-
+	//Body Parser method => Client send json to server
 	type Person struct {
 		Name string `json:"name"`
 		Pass string `json:"pass"`
@@ -25,7 +32,7 @@ func main() {
 			return err
 		}
 
-		log.Println(p.Name) // john
+		log.Println(p.Name) // john : log can show timestamp
 		log.Println(p.Pass) // doe
 		str := p.Name + p.Pass
 		return c.JSON(str)
@@ -36,14 +43,12 @@ func main() {
 		str := "hello ==> " + c.Params("name")
 		return c.JSON(str)
 	})
- 
-	app.Post("/inet", func(c *fiber.Ctx) error {
-        a := c.Query("search")
-        str := "my search is  " + a
-        return c.JSON(str)
-    })
 
- 
+	app.Post("/inet", func(c *fiber.Ctx) error {
+		a := c.Query("search")
+		str := "my search is  " + a
+		return c.JSON(str)
+	})
 
 	app.Listen(":3000")
 }
